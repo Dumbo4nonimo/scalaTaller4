b07df8cb81ae35f94403c00bfd47cf5a380966d2
Your Name <you@example.com>
2025-04-24 00:44:06 -0500

    Second commit - 5 functions implemented

--- src/main/scala/Anagramas/Package.scala
+++ src/main/scala/Anagramas/Package.scala
@@ -21,9 +21,24 @@ object Package {
     diccionario.groupBy(palabra => lOcPal(palabra))
   }
 
-  def anagramasDePalabra(palabra: Palabra): List[Palabra] = {
-    diccionarioPorOcurrencias.getOrElse(lOcPal(palabra), List())
+  def anagramasDePalabra(pal: Palabra): List[Palabra] = {
+    diccionarioPorOcurrencias.getOrElse(lOcPal(pal), List())
   }
+
+  def combinaciones(IOcurrencias: Ocurrencias): List[Ocurrencias] = {
+    var resultado = List(List.empty[(Char, Int)])
+    for ((char, max) <- IOcurrencias) {
+      val nuevos = for {
+        cantidad <- 1 to max
+        combinacion <- resultado
+      } yield (char, cantidad) :: combinacion
+
+      resultado = resultado ++ nuevos
+    }
+    resultado
+  }
+
+
 
 
   def main(args: Array[String]): Unit = {
@@ -37,6 +52,18 @@ object Package {
     println(anagramasDePalabra("aroma")) //porque cada palabra es anagrama de si mismo
     println(anagramasDePalabra("concha")) //ni siquiera debe aparecer entoces es vacio
     print(anagramasDePalabra("amor")) //aqui si aparecen varios
+    println("---------------------")
+    val entrada = List(('a', 2), ('b', 2))
+    val resultado = combinaciones(entrada)
+    resultado.foreach(println)
+    println("---------------------")
+    val entrada2 = List(('m', 1), ('n', 1), ('o', 1))
+    val resultado2 = combinaciones(entrada2)
+    resultado2.foreach(println)
+    println("---------------------")
+
+
+
 
     //nota: anagramasDePalabra es parecido a lazy pero no te dejes engañar, tienen diferencias
 
